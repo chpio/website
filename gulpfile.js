e9ba70ae8fe152fb93cc04fc19562e5ddf9010c6
Thomas Heck <t@b128.net>
2014-05-11 17:13:49 +0200

    concat von JS & CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gzip = require('gulp-gzip');
 var minifyHTML = require('gulp-minify-html');
 var website = require('gulp-website');
+var concat = require('gulp-concat');
 
 var gzipOptions =
   {
@@ -13,14 +14,39 @@ var minifyHTMLOptions =
     conditionals: true,
   };
 
-gulp.task('default',
+
+gulp.task('html',
   function()
   {
     gulp.src('./src/content/**/*.html')
       .pipe(website())
-      //.pipe(minifyHTML(minifyHTMLOptions))
-      //.pipe(gulp.dest('./build/'))
-      //.pipe(gzip(gzipOptions))
-      .pipe(gulp.dest('./build/'));
+      .pipe(minifyHTML(minifyHTMLOptions))
+      .pipe(gulp.dest('./build'))
+      .pipe(gzip(gzipOptions))
+      .pipe(gulp.dest('./build'));
   }
 );
+
+gulp.task('js',
+  function()
+  {
+    gulp.src(['./src/static/js/jquery.min.js', './src/**/*.js'])
+      .pipe(concat('all.js'))
+      .pipe(gulp.dest('./build/static'))
+      .pipe(gzip(gzipOptions))
+      .pipe(gulp.dest('./build/static'));
+  }
+);
+
+gulp.task('css',
+  function()
+  {
+    gulp.src('./src/**/*.css')
+      .pipe(concat('all.css'))
+      .pipe(gulp.dest('./build/static'))
+      .pipe(gzip(gzipOptions))
+      .pipe(gulp.dest('./build/static'));
+  }
+);
+
+gulp.task('default', ['html', 'js', 'css']);
